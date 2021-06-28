1f357027408b0bb7e3395afe45f4f0887d2c5b15
Joe Porrazzo <joepa2624@me.com>
2021-06-28 15:10:14 -0700

    added votes and comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -25,8 +41,22 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -47,7 +77,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    // expects {title: 'Apple is Going Out of Business!', post_url: 'https://applenews.com/press', user_id: 1}
+    // expects {title: 'Apple is going out of business!', post_url: 'https://applenew.com', user_id: 1}
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
@@ -57,6 +87,16 @@ router.post('/', (req, res) => {
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
+        });
+});
+
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
         });
 });
 
@@ -83,6 +123,7 @@ router.put('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
 
 router.delete('/:id', (req, res) => {
     Post.destroy({
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -18,7 +18,27 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
@@ -34,6 +54,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    // expects {username: 'josephporrazzo', email: 'josephporrazzo@gmail.com', password: 'password1234'}
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -47,7 +68,7 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    // expects "username": "josephporrazzo", "email address": "josephporrazzo@gmail.com", "password": "password1234"
+    // expects {email: 'josephporrazzo@gmail.com', password: 'password1234'}
     User.findOne({
         where: {
             email: req.body.email
@@ -59,6 +80,7 @@ router.post('/login', (req, res) => {
         }
 
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
@@ -69,6 +91,7 @@ router.post('/login', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+    // expects {username: 'josephporrazzo', email: 'josephporrazzo@gmail.com', password: 'password1234'}
     User.update(req.body, {
         individualHooks: true,
         where: {
