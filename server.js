3404f23fbd9369d9e5bbd36da7b0126fce2fa752
Joe Porrazzo <joepa2624@me.com>
2021-06-29 14:29:45 -0700

    partials

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
