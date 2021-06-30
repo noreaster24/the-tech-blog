965b0a73e53785f4e723e4ea6fc5e4ec46f04df8
Joe Porrazzo <joepa2624@me.com>
2021-06-29 21:33:10 -0700

    updated server connection

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const helpers = require('./utils/helpers');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
